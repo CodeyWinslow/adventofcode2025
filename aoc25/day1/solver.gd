33ba9b8992805b7cd6854bfa222f1432198cb5bb
Codey <codeywinslow@gmail.com>
2025-12-08 02:41:59 -0800

    Fixed build issues Day 1

--- aoc25/day1/solver.gd
+++ aoc25/day1/solver.gd
@@ -1,10 +1,12 @@
 extends Node
 class_name Solver
 
-var moves:Array
-var current_move : int = 0
-var current_position : int = 50
-var number_zeroed : int = 0
+var moves: Array
+var current_move: int = 0
+var current_position: int = 50
+var number_zeroed: int = 0
+
+const FILE_PATH = "res://data/day1/input.txt"
 
 func _ready():
 	_read_input()
@@ -20,8 +22,8 @@ func GetNextMove():
 func IsFinished():
 	return current_move >= len(moves)
 
-func process_move(position:int, move:Move):
-	var delta : int = 1
+func process_move(position: int, move: Move):
+	var delta: int = 1
 	if move.direction == Move.MoveDirection.Left:
 		delta = -1
 		
@@ -31,14 +33,12 @@ func process_move(position:int, move:Move):
 func _read_input():
 	var text := ""
 	
-	var script_dir = get_script().resource_path.get_base_dir()
-	var file_path = script_dir.path_join("input.txt")
-	
-	var f := FileAccess.open(file_path, FileAccess.READ)
+	var f := FileAccess.open(FILE_PATH, FileAccess.READ)
 	if f:
 		text = f.get_as_text()
 	else:
-		push_error("Could not open file: %s" % file_path)
+		push_error("Could not open file: %s" % FILE_PATH)
+		DisplayServer.dialog_show("Error", "Could not open file: %s" % FILE_PATH, PackedStringArray(["OK"]), error_message_ok_pressed)
 		return
 
 	var lines := text.split("\n", false)
@@ -47,9 +47,12 @@ func _read_input():
 		_process_input_line(line)
 		
 	print("Parsed %d moves from input file" % [len(moves)])
+
+func error_message_ok_pressed():
+	pass
 		
-func _process_input_line(line:String):
-	var move : Move = Move.new()
+func _process_input_line(line: String):
+	var move: Move = Move.new()
 	if line.begins_with('L'):
 		move.direction = Move.MoveDirection.Left
 	else:
